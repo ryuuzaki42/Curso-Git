c9f4f31ed14b4a8b0ced13a6a921f34e4e270d7d
ryuuzaki42 <joao42lbatista@gmail.com>
2019-06-13 19:51:38 -0300

    Add Soma

---
+++ projeto.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int soma(int a, int b){
+   return (a+b);
+}
+
+int main(){
+   int a, b;
+   printf("Digite o valor de a: ");
+   scanf("%d", &a);
+
+   printf("Digite o valor de b: ");
+   scanf("%d", &b);
+
+   printf("Soma: %d", soma(a, b));
+
+   return 0;
+}
