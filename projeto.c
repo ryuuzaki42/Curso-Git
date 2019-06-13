1c3f69d897c5b890533acc129716d63cb1be6d21
ryuuzaki42 <joao42lbatista@gmail.com>
2019-06-13 19:53:54 -0300

    Add Sub

--- projeto.c
+++ projeto.c
@@ -4,6 +4,10 @@ int soma(int a, int b){
    return (a+b);
 }
 
+int subtracao(int a, int b){
+   return (a-b);
+}
+
 int main(){
    int a, b;
    printf("Digite o valor de a: ");
@@ -13,6 +17,7 @@ int main(){
    scanf("%d", &b);
 
    printf("Soma: %d", soma(a, b));
+   printf("Soma: %d", subtracao(a, b));
 
    return 0;
 }
