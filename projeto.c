e8f17921146f7049d47d7bc0d5b8a8bacda87820
ryuuzaki42 <joao42lbatista@gmail.com>
2019-06-13 19:58:54 -0300

    Add Divisao

--- projeto.c
+++ projeto.c
@@ -12,6 +12,10 @@ int multiplicacao(int a, int b){
    return (a*b);
 }
 
+int divisao(int a, int b){
+   return (a/b);
+}
+
 int main(){
    int a, b;
    printf("Digite o valor de a: ");
@@ -23,6 +27,8 @@ int main(){
    printf("\nSoma: %d", soma(a, b));
    printf("\nSubtracao: %d", subtracao(a, b));
    printf("\nMultiplicacao: %d", multiplicacao(a, b));
+   printf("\ndivisao: %d", divisao(a, b));
 
    return 0;
 }
+
